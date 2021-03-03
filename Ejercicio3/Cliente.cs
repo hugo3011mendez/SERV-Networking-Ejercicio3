125ea728f3181458a9442bd6257266384bd7b43e
Hugo Mendez <hugo3011mendez@gmail.com>
2021-03-03 14:29:54 +0100

    Hecha estructura nueva del ejercicio :
    - Implementadas funciones asignarNumeroRandom(), cuentaAtras(), mensajeATodos(), mensajeAUno(), desconectar(), comprobarGanador() y finalJuego()
    
    - Actualizada clase Cliente lanzando un hilo en su constructor para asignarle un número entre 1 y 20.

--- Ejercicio3/Cliente.cs
+++ Ejercicio3/Cliente.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
-using System.Threading;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Ejercicio3
 {
     class Cliente
     {
+        static readonly internal object m = new object();   //en cada acceso a clients --> recurso común
+
         private Socket sClient; // Atributo que representa el socket del cliente que se conecta al server
         public Socket SClient // Setter y Getter
         {
@@ -42,21 +44,6 @@ namespace Ejercicio3
         }
 
 
-        private bool listo;
-        public bool Listo
-        {
-            set
-            {
-                listo = value;
-            }
-
-            get
-            {
-                return listo;
-            }
-        }
-
-
         private int numero;
         public int Numero
         {
@@ -72,10 +59,52 @@ namespace Ejercicio3
         }
 
 
-        public Cliente(Socket s) // Constructor, donde paso como parámetro el socket y le doy valores junto al IPEndPoint
+        public Cliente(Socket s) // Constructor, donde paso como parámetro el socket
         {
+            // Le doy valores al Socket junto al IPEndPoint
             SClient = s;
             IeCliente = (IPEndPoint)s.RemoteEndPoint;
+
+            // Lanzo un hilo 
+            Thread t = new Thread(juego);
+            t.Start();
+        }
+
+
+        private void juego() // Función que realiza las acciones necesarias para el cliente a la hora de proporcionarle un número aleatorio
+        {
+            try
+            {
+                using (NetworkStream ns = new NetworkStream(SClient))
+                using (StreamReader sr = new StreamReader(ns))
+                using (StreamWriter sw = new StreamWriter(ns))
+                {
+                    // Defino un mensaje de bienvenida y se lo muestro
+                    string bienvenida = "Recibiras un numero del 1 al 20. Si tu numero es el mas alto seras el ganador!";
+                    sw.WriteLine(bienvenida);
+                    sw.Flush();
+
+                    Numero = Server.asignarNumeroRandom(this); // Le asigno un número random al cliente
+
+                    lock (Server.l)
+                    {
+                        Monitor.Pulse(Server.l);
+                    }
+
+                    while (true) // Mientras este cliente siga conectado...
+                    {
+                        lock (m)
+                        {
+                            Monitor.Wait(m);
+                        }
+                    }
+                }
+            }
+            catch (IOException) // Si ocurre una excepción, el cliente se desconecta
+            {
+                Server.desconectar(this); 
+            }
+
         }
     }
 }
--- Ejercicio3/Server.cs
+++ Ejercicio3/Server.cs
@@ -14,74 +14,204 @@ namespace Ejercicio3
 {
     class Server
     {
-        static readonly object l = new object(); // Creo el objeto del lock, porque los hilos están usando recursos iguales
-
+        static readonly internal object l = new object(); // Creo el objeto del lock, porque los hilos están usando recursos iguales
         static List<Cliente> clientes = new List<Cliente>(); // Creo una colección de clientes para ir guardando en ella los clientes que se conectan
 
+        private static List<int> numeros = new List<int>(); // Lista de números que han tocado
+        static Random generador = new Random(); // Generador de números aleatorios
 
-        static void funcionCliente(object cl)
+        private static DateTime tiempo; // Tiempo transcurrido
+        const int TIEMPOJUEGO = 15; // Constante que indica la duración de la cuenta atrás
+        private static int seg = TIEMPOJUEGO;
+        private static bool final = false;
+
+
+        internal static int asignarNumeroRandom(Cliente cl) // Le asigno un número entre 1 y 20 al cliente indicado como parámetro
         {
-            Cliente cliente = (Cliente)cl;
+            bool repetir = true; // Booleana para saber si se repite la asignación o no
+            bool numAsignado = false; // Booleana que comprueba si ya se le ha asignado un número aleatorio al cliente
 
-            Random generador = new Random();
-            cliente.Numero = generador.Next(1, 21);
+            int num = 1; // Número a asignar
 
-            bool salir = false; // Creo variable para indicar si el cliente debe salir o no del juego
-
-            using (NetworkStream ns = new NetworkStream(cliente.SClient))
-            using (StreamReader sr = new StreamReader(ns))
-            using (StreamWriter sw = new StreamWriter(ns))
+            while (repetir)
             {
-                while (!salir)
+                num = generador.Next(1, 21); // Le doy un valor aleatorio dentro del rango que queremos
+
+                lock (l)
                 {
-                    if (clientes.Count >= 2)
+                    if (!numeros.Contains(num)) // Si la colección de números no tiene a este número...
                     {
-                        int cont = 0;
+                        repetir = false; // Ya no hace falta repetir la asignación del número
+                        numeros.Add(num); // Áñado el número a la colección
+                        numAsignado = true; // El número ha sido asignado correctamente
+                    }
+                    if (numAsignado) // Si se ha asignado correctamente el número...
+                    {
+                       cl.Numero = num; // Establezco el número asignado en su propiedad correspondiente en el cliente
+                       //mensajeAUno(c, "Tu numero es " + c.Numero);
+                    }
+                }
+            }
 
-                        lock (l)
+            return cl.Numero; // Devuelvo el número asignado al cliente
+        }
+
+
+        private static void cuentaAtras() // Función donde realizo la cuenta atrás
+        {
+            tiempo = DateTime.Now.AddSeconds(seg + 1); // Añado un segundo de cada vez al tiempo transcurrido
+
+            while (!final) // Mientras no llegue el final de la cuenta atrás...
+            {
+                lock (l)
+                {
+                    if (!final) // Si la cuenta atrás aún no ha finalizado...
+                    {
+                        DateTime now = DateTime.Now; // Declaro y establezco el valor de una variable como el momento actual
+                        TimeSpan dif = tiempo - now; // Declaro y establezco el valor de una variable como la diferencia de tiempo entre el tiempo transcurrido y el momento actual
+
+                        // Si la diferencia de tiempo entre el tiempo transcurrido y el momento actual se puede dividir en segundos y es igual al tiempo restante para que acabe la cuenta atrás
+                        if (dif.Milliseconds % 1000 == 0 && dif.Seconds == seg) 
                         {
-                            foreach(Cliente cli in clientes)
-                            {
-                                if (cli.Listo)
-                                {
-                                    cont++;
-                                }
-                            }
+                            string tiempoRestante = string.Format($"{dif:mm\\:ss}"); // Formateo el tiempo restante en una cadena
+                            mensajeATodos(tiempoRestante); // Y se la muestro a todos los clientes
 
+                            seg --; // Reduzco los segundos restantes para acabar la cuenta atrás
                         }
 
-                        if (cont == clientes.Count)
+
+                        if (dif.Seconds == 0) // Si no existe diferencia de tiempo entre el tiempo transcurrido y el momento actual...
                         {
-                            // Empieza la cuenta atrás
+                            string tiempoRestante = string.Format($"{dif:mm\\:ss}"); // Formateo el tiempo restante en una cadena
+                            mensajeATodos(tiempoRestante); // Y se la muestro a todos los clientes
 
-                            int numGanador = 0;
-
-                            lock (l)
-                            {
-                                for (int i = 0; i < clientes.Count; i++)
-                                {
-                                    if (clientes[i].Numero >= numGanador)
-                                    {
-                                        numGanador = clientes[i].Numero;
-                                    }
-                                }
-                            }
-
-                            if (numGanador == cliente.Numero)
-                            {
-                                sw.WriteLine("ERES EL GANADOR!!!");
-                            }
-                            else
-                            {
-                                sw.WriteLine("No has ganado...\n");
-                                sw.WriteLine("Tu número : " + cliente.Numero);
-                                sw.WriteLine("Número ganador : " + numGanador);
-                            }
+                            mensajeATodos("SE ACABO EL TIEMPO!!"); // Mando el mensaje a todos los clientes de que se ha acabado el tiempo
+                            comprobarGanador(); // Compruebo el ganador
+                            finalJuego(); // Y acabo la partida
                         }
                     }
                 }
             }
         }
+
+
+        private static void mensajeATodos(string msg) // Envío un  mensaje a todos los clientes desde el servidor
+        {
+            if (msg != null)
+            {
+                lock (l)
+                {
+                    for (int i = 0; i < clientes.Count; i++)
+                    {
+                        try
+                        {
+                            using (NetworkStream ns = new NetworkStream(clientes[i].SClient))
+                            using (StreamReader sr = new StreamReader(ns))
+                            using (StreamWriter sw = new StreamWriter(ns))
+                            {
+                                sw.WriteLine(msg);
+                                sw.Flush();
+                            }
+                        }
+                        catch (IOException) // Si ocurre una excepción, desconecto al cliente que tenga el error y muestro el mensaje de error
+                        {
+                            desconectar(clientes[i]);
+
+                            Console.WriteLine("Error mensajeando a todos los clientes : " + msg);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private static void mensajeAUno(Cliente cl, string msg) // Envío un mensaje a un cliente desde el servidor
+        {
+            if (msg != null)
+            {
+                lock (l)
+                {
+                    try
+                    {
+                        using (NetworkStream ns = new NetworkStream(cl.SClient))
+                        using (StreamReader sr = new StreamReader(ns))
+                        using (StreamWriter sw = new StreamWriter(ns))
+                        {
+                            sw.WriteLine(msg);
+                            sw.Flush();
+                        }
+                    }
+                    catch (IOException) // Si ocurre una excepción, desconecto al cliente correspondiente
+                    {
+                        Console.WriteLine("Error mensajeando al cliente\t" + cl.IeCliente.Address + ":" + cl.IeCliente.Port);
+
+                        desconectar(cl);
+                    }
+                }
+            }
+        }
+
+
+        // Desconecto al cliente indicado como parámetro cerrando su socket, eliminando su número de la colección y eliminándolo de la colección de clientes
+        internal static void desconectar(Cliente cl) 
+        {
+            lock (l)
+            {
+                cl.SClient.Close();
+
+                numeros.Remove(cl.Numero);
+
+                clientes.Remove(cl);
+            }
+        }
+
+
+        static void comprobarGanador()
+        {
+
+            if (clientes.Count > 0)
+            {
+                Cliente ganador = clientes[0]; // Primero determino al primer cliente como ganador
+
+                for (int i = 0; i < clientes.Count; i++) // Recorro todos los clientes conectados
+                {
+                    mensajeAUno(clientes[i], "Tu numero es " + clientes[i].Numero); // Muestro su propio número al cliente correspondiente
+
+                    if (clientes[i].Numero >= ganador.Numero) // Si el número del cliente por el que está pasando es mayor al número del cliente establecido como ganador... 
+                    {
+                        ganador = clientes[i]; // El cliente actual se establece como ganador
+                    }
+                }
+
+
+                for (int i = 0; i < clientes.Count; i++) // Vuelvo a recorrer los clientes conectados
+                {
+                    if (clientes[i] != ganador) // Compruebo si el cliente actual no es el ganador
+                    {
+                        mensajeAUno(clientes[i], "El numero ganador es : " + ganador.Numero);
+
+                        mensajeAUno(clientes[i], "No has ganado"); // Mando el mensaje de que no ha ganado al cliente actual
+                    }
+                }
+
+                mensajeAUno(ganador, "HAS GANADO!!"); // Mando el mensaje de que ha ganado al cliente ganador
+            }
+        }
+
+        private static void finalJuego() // Acciones a realizar cuando se acaba el juego
+        {
+            if (clientes.Count > 0)
+            {
+                for (int i = 0; i < clientes.Count; i++) // Recorro la colección de clientes y los desconecto
+                {
+                    desconectar(clientes[i]);
+                }
+
+                seg = TIEMPOJUEGO; // Reinicio la variable seg
+            }
+        }
+
+
 
 
         static void Main(string[] args)
@@ -120,7 +250,7 @@ namespace Ejercicio3
                 clientes.Add(new Cliente(sClient));
 
                 // Uso la funcionCliente para el hiloCliente y lo lanzo pasándole el último cliente añadido a la colección como parámetro
-                Thread hiloCliente = new Thread(funcionCliente);
+                Thread hiloCliente = new Thread(cuentaAtras);
                 hiloCliente.Start(clientes[clientes.Count - 1]);
             }
         }
